45579acd1527e9aaba9961e7d1f6a53af68c07f5
claralam <claralam@andrew.cmu.edu>
2022-10-01 17:39:51 -0400

    Added new table to database and changed database version in config

---
+++ docs-core/src/main/resources/db/update/dbupdate-028-0.sql
@@ -0,0 +1,6 @@
+create cached table T_SCORE ( SCORE_ID_C varchar(36) not null, SCORE_IDDOC_C varchar(36) not null, SCORE_IDUSER_C varchar(36) not null, SCORE_SKILLS_C int, SCORE_EXPERIENCE_C int, SCORE_TRANSCRIPTGPA_C int, SCORE_MATCH_C int, SCORE_CREATEDATE_D datetime, primary key (SCORE_ID_C) );
+
+alter table T_SCORE add constraint FK_SCORE_IDDOC_C foreign key (SCORE_IDDOC_C) references T_DOCUMENT (DOC_ID_C) on delete restrict on update restrict;
+alter table T_SCORE add constraint FK_SCORE_IDUSER_C foreign key (SCORE_IDUSER_C) references T_USER (USE_ID_C) on delete restruct on update restruct;
+
+update T_CONFIG set CFG_VALUE_C = '28' where CFG_ID_C = 'DB_VERSION';
